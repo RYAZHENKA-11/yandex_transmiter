6874599370d71587419e1f939b5aec2e31a57298
RYAZHENKA-11 <122489404+RYAZHENKA-11@users.noreply.github.com>
2023-04-18 14:03:42 +0300

    Add files via upload

---
+++ File-transfer-unit-WEB-main/YANDEXPROJECT_3_feat.Slave/data/users.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    uid = sqlalchemy.Column(sqlalchemy.Integer,
+                            primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    phonenum = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, 
+                              index=True, unique=True, nullable=True)
+    login = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
