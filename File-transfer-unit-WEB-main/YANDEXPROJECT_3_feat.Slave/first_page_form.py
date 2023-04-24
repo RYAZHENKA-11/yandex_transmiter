d4708b591f117ab75efba40f1d76f00b8a29e695
RYAZHENKA-11 <122489404+RYAZHENKA-11@users.noreply.github.com>
2023-04-24 15:15:28 +0300

    Delete File-transfer-unit-WEB-main/YANDEXPROJECT_3_feat.Slave directory

--- File-transfer-unit-WEB-main/YANDEXPROJECT_3_feat.Slave/first_page_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class FirstForm(FlaskForm):
-    cod = StringField('Введите код для скачивания:', validators=[DataRequired()])
-    download = SubmitField('Скачать')
